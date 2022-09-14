512217ee2abbcbe2af886c7c0c138b813917fabf
Sandra Esquivel <sandra.esquivel.mt@usa.edu.co>
2022-09-13 19:42:10 -0500

    Agregando Clase Ejercicio2.java

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author User
+ */
+public class Ejercicio2 {
+    public static void otro(){
+        System.out.println("YO");
+    }
+}
